d60b5cec129ff3c68780b05f177780bcf43918b3
이상호 <sangho@isanghoui-Macmini.local>
2023-10-29 15:41:29 +0900

    20231029

--- app/src/main/java/com/example/mqtt_test/MainActivity.java
+++ app/src/main/java/com/example/mqtt_test/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.mqtt_test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.media.session.MediaController;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
@@ -19,12 +28,25 @@ public class MainActivity extends AppCompatActivity {
     private int cnt = 0;
     TextView msgview;
     private MqttClient mqttClient = null;
+
+    Uri video_uri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         msgview = (TextView) findViewById(R.id.mqtt_message);
         msgview.setText(msg+Integer.toString(cnt));
+
+        video_uri = Uri.parse("android.resource://com.example.mqtt_test/" +
+                + R.raw.middle);
+
+        VideoView video_View = (VideoView) findViewById(R.id.videoView);
+        video_View.setVideoURI(video_uri);
+        video_View.start();
+        video_View.setVisibility(View.VISIBLE);
 
         try {
             mqttClient = new MqttClient(ServerIP, MqttClient.generateClientId(), null);
@@ -48,6 +70,29 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             msgview.setText(msg+Integer.toString(cnt));
+
+                            if (msg.equals("low")){
+                                video_uri = Uri.parse("android.resource://com.example.mqtt_test/" +
+                                        R.raw.low);
+                            } else if (msg.equals("middle")) {
+                                video_uri = Uri.parse("android.resource://com.example.mqtt_test/" +
+                                        R.raw.middle);
+                            }else if (msg.equals("high")) {
+                                video_uri = Uri.parse("android.resource://com.example.mqtt_test/" +
+                                        R.raw.high);
+                            }
+                            else if (msg.equals("safety")) {
+                                video_uri = Uri.parse("android.resource://com.example.mqtt_test/" +
+                                        R.raw.safety);
+                            }else if (msg.equals("advertise")) {
+                                video_uri = Uri.parse("android.resource://com.example.mqtt_test/" +
+                                        R.raw.advertise);
+                            }
+
+
+                            video_View.setVideoURI(video_uri);
+                            video_View.start();
+                            video_View.setVisibility(View.VISIBLE);
                         }
                     });
                 }
@@ -62,4 +107,8 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
+
+
 }
